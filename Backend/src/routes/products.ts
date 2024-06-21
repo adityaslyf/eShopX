2c65893b2e4b5e1bf7d7d65da19de1a67a52b8f8
adityaslyf <aditya.varshneymail@gmail.com>
2024-06-22 02:20:31 +0530

    merging latest code in backend branch

--- Backend/src/routes/products.ts
+++ Backend/src/routes/products.ts
@@ -8,13 +8,12 @@ import {
   getAdminProducts,
 } from "../controllers/product.js";
 import { singleUpload } from "../middleware/multer.js";
-import { get } from "http";
 
 const app = express.Router();
 
 app.post("/new", singleUpload, newProduct);
 app.get("/latest", getLatestProducts);
 app.get("/categories", getCategories);
-app.get("/admin-products" , getAdminProducts);
+app.get("/admin-products", getAdminProducts);
 
 export default app;
