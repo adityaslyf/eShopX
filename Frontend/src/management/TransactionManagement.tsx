9dcdf57c36802fa4cb7b84b745cefbc18095bc9d
adityaslyf <aditya.varshneymail@gmail.com>
2024-06-22 10:29:46 +0530

    comment out transaction managmenet

--- Frontend/src/management/TransactionManagement.tsx
+++ Frontend/src/management/TransactionManagement.tsx
@@ -1,47 +1,47 @@
-import Sidebar from "../components/Sidebar";    
-import { OrderItemType, OrderType } from '../types';
+// import Sidebar from "../components/Sidebar";    
+// import { OrderItemType, OrderType } from '../types';
 
 
-const orderItems: OrderItemType[] = [
-    {
-        id: 1,
-        name: "Product 1",
-        price: 100,
-        quantity: 2
-    },
-    {
-        id: 2,
-        name: "Product 2",
-        price: 200,
-        quantity: 1
-    },
-    {
-        id: 3,
-        name: "Product 3",
-        price: 300,
-        quantity: 3
-    }
-];
+// const orderItems: OrderItemType[] = [
+//     {
+//         id: 1,
+//         name: "Product 1",
+//         price: 100,
+//         quantity: 2
+//     },
+//     {
+//         id: 2,
+//         name: "Product 2",
+//         price: 200,
+//         quantity: 1
+//     },
+//     {
+//         id: 3,
+//         name: "Product 3",
+//         price: 300,
+//         quantity: 3
+//     }
+// ];
 
-const TransactionManagement = () => {
-    const [ order , setOrder ] = useState<OrderType[]>([]);
-    name: "John Doe",
-    address: "123 Main St",
-    city : "New York",
-    state : "NY",
-    country : "USA",
-    pincode : 10001,
-  status: "Processing",
-  discount : 10,
-  subtotal: 1000,
-  tax: 100,
-  total : 1100,
+// const TransactionManagement = () => {
+//     const [ order , setOrder ] = useState<OrderType[]>([]);
+//     name: "John Doe",
+//     address: "123 Main St",
+//     city : "New York",
+//     state : "NY",
+//     country : "USA",
+//     pincode : 10001,
+//   status: "Processing",
+//   discount : 10,
+//   subtotal: 1000,
+//   tax: 100,
+//   total : 1100,
 
-  return (
-    <div>
-      <Sidebar />
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <Sidebar />
+//     </div>
+//   )
+// }
 
-export default TransactionManagement
+// export default TransactionManagement
