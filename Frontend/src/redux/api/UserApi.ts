0bd79428da180c327a8bae7ba089aab25277d7c9
adityaslyf <aditya.varshneymail@gmail.com>
2024-09-22 21:43:34 +0530

    deployment check

--- Frontend/src/redux/api/UserApi.ts
+++ Frontend/src/redux/api/UserApi.ts
@@ -1,18 +1,18 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { server } from "../store";
-import { MessageResponse } from "../../types/api-types";
-import { User } from "../../types/types";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { server } from "../store";
+// import { MessageResponse } from "../../types/api-types";
+// import { User } from "../../types/types";
 
-export const userApi = createApi({
-  reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${server}/api/v1/user`,
-  }),
-  endpoints: (builder) => ({
-    login: builder.mutation<MessageResponse , User >({
-      query: (user) => ({ url: "new", method: "POST", body: user }),
-    }),
-  }),
-});
+// export const userApi = createApi({
+//   reducerPath: "userApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: `${server}/api/v1/user`,
+//   }),
+//   endpoints: (builder) => ({
+//     login: builder.mutation<MessageResponse , User >({
+//       query: (user) => ({ url: "new", method: "POST", body: user }),
+//     }),
+//   }),
+// });
 
-export const { useLoginMutation } = userApi;
+// export const { useLoginMutation } = userApi;
--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -1,7 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit';
+// import {configureStore} from '@reduxjs/toolkit';
+// import { userApi } from './api/UserApi';
 
-export const server = import.meta.env.VITE_SERVER_URL;
+// export const server = import.meta.env.VITE_SERVER_URL;
 
-export const store = configureStore({
-    reducer: {}, // Add reducers here
-});
+// export const store = configureStore({
+//     reducer: {
+//         [userApi.reducerPath]: userApi.reducer,
+
+//     }, // Add reducers here
+// });
